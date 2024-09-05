87f9d1956ca31d134a4a1fd4a82f4ea258f6d64c
Tarik Alaiwi <tarik.alaiwi.dev@gmail.com>
2024-09-05 16:49:55 +0200

    add some structure

--- vue_app/src/views/HomeView.vue
+++ vue_app/src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
-    home
+    
+    <div class="columns is-mobile grid-xl">
+      <div class="column is-flex is-flex-direction-column is-align-items-center">
+        <div class="">29.09.2022</div>
+        <figure class="image is-3by2"><img src="" alt=""></figure>
+        <div class="text-centered">inference</div>
+      </div>
+      <div class="column is-flex is-flex-direction-column is-align-items-center">
+        <div class="">TODAY</div>
+        <figure class="image is-3by2"><img src="" alt=""></figure>
+        <button class="button is-active">UPLOAD</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
